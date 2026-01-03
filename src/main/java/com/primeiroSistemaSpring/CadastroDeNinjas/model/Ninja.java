7fc6251b4422b20ac4c61d99ee71bbb2f5e55a9d
carlinn1 <carlos_-425@hotmail.com>
2026-01-03 13:14:22 -0300

    Configurando Ninja.java criando entidades no banco!

--- src/main/java/com/primeiroSistemaSpring/CadastroDeNinjas/model/Ninja.java
+++ src/main/java/com/primeiroSistemaSpring/CadastroDeNinjas/model/Ninja.java
@@ -1,48 +1,24 @@
 package com.primeiroSistemaSpring.CadastroDeNinjas.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ninja {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Double id;
-    String nome;
-    String email;
-    int idade;
+    private Double id;
+    private String nome;
+    private String email;
+    private int idade;
 
-    public Ninja() {
-
-    }
-    public Ninja(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
